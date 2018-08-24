265a23f451d8e394213c8b5be01146875e3c73ae
Evan <blesbparker@gmail.com>
2018-08-24 18:37:49 -0400

    End of Section 9

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '../../../../../../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react-redux-toastr/node_modules/redux'
 import testReducer from '../../features/testArea/testReducer';
 import eventReducer from '../../features/event/eventReducer'
+import {reducer as FormReducer} from 'redux-form'
 
 const rootReducer = combineReducers({
+    form: FormReducer,
     test: testReducer,
     events: eventReducer
 })
